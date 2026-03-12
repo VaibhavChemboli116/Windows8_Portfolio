87ae5039f4c8f91e980479324f7f61728ef87c87
Vaibhav Chemboli <vaibhavchemboli@Vaibhavs-MacBook-Air.local>
2026-03-12 01:26:46 -0400

    Update experience section

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -271,22 +271,15 @@ export const projects: Project[] = [
           flexDirection: 'row',
           justifyContent: 'center',
           alignItems: 'center',
-          gap: '6px',
           marginBottom: '10px',
           flexWrap: 'wrap'
         }
       },
         React.createElement('span', null,
-          React.createElement('a', { href: 'https://www.yu.edu/katz', target: '_blank', rel: 'noopener noreferrer', style: { color: '#703cb0', textDecoration: 'underline' } }, 'Yeshiva University')
-        ),
-        React.createElement('span', null, ','),
-        React.createElement('span', null, 'Manhattan, New York, United States'),
-        React.createElement('span', null, ','),
-        React.createElement('span', null, 'On-site'),
-        React.createElement('span', null, ','),
-        React.createElement('span', null, 'Feb 2025 - Dec 2025'),
-        React.createElement('span', null, ','),
-        React.createElement('span', { style: { fontWeight: 600 } }, 'Part-time')
+          React.createElement('a', { href: 'https://www.yu.edu/katz', target: '_blank', rel: 'noopener noreferrer', style: { color: '#703cb0', textDecoration: 'underline' } }, 'Yeshiva University'),
+          ' • Manhattan, New York, United States • On-site • Feb 2025 - Dec 2025 • ',
+          React.createElement('span', { style: { fontWeight: 600 } }, 'Part-time')
+        )
       ),
       React.createElement('div', { style: { margin: '0 0 10px 0', textAlign: 'left' } },
         React.createElement('strong', null, 'Responsibilities & Achievements:'),
